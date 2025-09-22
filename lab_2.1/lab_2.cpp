0137e0cc4b54ffaa19aedcafd19feafafd2c5fc0
NastYa <harbar.anast@gmail.com>
2025-09-22 20:58:25 +0300

    fist master commit

---
+++ lab_2.1/lab_2.cpp
@@ -0,0 +1,32 @@
+// Lab_02.cpp
+// Гарбар Анастасія
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 0.2
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+	double a; // вхідний параментр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+
+	cout << "a = "; 
+	cin >> a;
+
+	z1 = (sin(2*a) + sin(5*a) - sin(3*a)) / (cos(a) + 1 - 2*sin(2*a) * sin(2*a));
+	z2 = 2 * sin(a);
+
+	// вивід
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
